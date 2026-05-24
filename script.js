f2e8277b8d9654ef6f3290da49cece6cb176e23a
cabelinh0o1 <davidsoncamargo945@gmail.com>
2026-05-24 20:17:02 -0300

    segundo commit do JS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let conta = 0
 let pessoas = 0
+let procentagem = 0
 
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
@@ -26,4 +27,15 @@ function receberQuantidadePessoas(evento){
         divErro.setAttribute("id", "")
         pessoas = Number(evento.target.value)
     }
+}
+
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click", receberPorcentagem)
+})
+
+function receberPorcentagem(evento){
+    console.log(evento.target.value)
+
+    //procentagem = Number(evento.target.value) / 100 
 }
