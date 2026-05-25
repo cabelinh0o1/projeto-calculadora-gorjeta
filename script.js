ceab3595475185f0f74484a316f4f44f66805fe7
cabelinh0o1 <davidsoncamargo945@gmail.com>
2026-05-24 21:02:06 -0300

    sexto commit do JS e projeto FINALIZADO

--- script.js
+++ script.js
@@ -88,4 +88,7 @@ function limpar(){
     document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0.00"
     document.querySelector(".total > strong").innerHTML = "R$ 0.00"
 
+    conta = 0
+    procentagem = 0
+    pessoas = 0
 }
